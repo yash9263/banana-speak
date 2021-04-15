2bf8e6d8a86647c176bad2e34f0cdbebb424c81a
yash9263 <yashwant8530@gmail.com>
2021-04-15 22:56:58 +0530

    added fetch

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 var btnTranslate = document.querySelector("#btn-translate");
-btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("clicked");
-})
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something wrong with server! try again after some time");
+}
+
+function clickEventHandler() {
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(res => res.json())
+        .then(json => {
+            outputDiv.innerText = json.contents.translated;
+        })
+        .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
